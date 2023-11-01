3b6e95147ca3aff4f147f7043feee5f2ccfb56d2
Gian-Mattia Ramseier <gianni@MacBook-Air-von-Gian-Mattia.local>
2023-11-01 21:32:32 +0100

    fixed issue in zahlenundfakten

--- components/ZahlenUndFakten.jsx
+++ components/ZahlenUndFakten.jsx
@@ -10,34 +10,34 @@ const ZahlenUndFakten = () => {
             <div>
                 <Counter targetNumber={390} />
                 <h1 className="text-blue-800 mb-5 text-5xl">Scharen</h1>
-                <p className='text-2xl'>Über 390 Scharen (lokale Gruppen) bieten schweiz­weit das Angebot «Freizeit­spass und Lebens­schule» an und schaffen damit Lebens­freu(n)de pur!</p>
+                <p className='text-2xl'>Über 390 Scharen (lokale Gruppen) bieten schweizweit das Angebot «Freizeitspass und Lebensschule» an und schaffen damit Lebensfreu(n)de pur!</p>
             </div>
             <div>
                 <Counter targetNumber={32500} />
                 <h1 className="text-blue-800 mb-5 text-5xl">Mitglieder</h1>
-                <p className='text-2xl'>Die Jubla zählt schweiz­weit über 32'500 Mitglieder. Davon sind 22'000 Kinder und Jugend­liche, die regel­mässig an Gruppen­stunden, Schar­anlässen und Lagern teilnehmen. Weitere über 10'500 Jugend­liche und junge Erwachsene engagieren sich als Leitungs­personen oder als Aktive im Verband ehren­amtlich für die Jubla.</p>
+                <p className='text-2xl'>Die Jubla zählt schweizweit über 32500 Mitglieder. Davon sind 22000 Kinder und Jugendliche, die regelmässig an Gruppenstunden, Scharanlässen und Lagern teilnehmen. Weitere über 10500 Jugendliche und junge Erwachsene engagieren sich als Leitungspersonen oder als Aktive im Verband ehrenamtlich für die Jubla.</p>
             </div>
             <div>
                 <Counter targetNumber={3000000} />
                 <h1 className="text-blue-800 mb-5 text-5xl">Stunden</h1>
-                <p className='text-2xl'>In der Jubla leisten Jugendliche und junge Erwachsene schweiz­weit über 3 Millionen Stunden ehren­amtliches Engagement pro Jahr. Damit gestalten sie unsere Gesell­schaft aktiv mit und eignen sich wert­volle Kompetenzen an.</p>
+                <p className='text-2xl'>In der Jubla leisten Jugendliche und junge Erwachsene schweizweit über 3 Millionen Stunden ehrenamtliches Engagement pro Jahr. Damit gestalten sie unsere Gesellschaft aktiv mit und eignen sich wertvolle Kompetenzen an.</p>
             </div>
         </div>
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 mb-8'>
             <div>
                 <Counter targetNumber={450} />
                 <h1 className="text-blue-800 mb-5 text-5xl">Lager</h1>
-                <p className='text-2xl'>Schweizweit finden jährlich über 400 Lager statt. Sie bilden neben den Aktivitäten unter dem Jahr den Höhe­punkt einer jeden Jubla-Schar.</p>
+                <p className='text-2xl'>Schweizweit finden jährlich über 400 Lager statt. Sie bilden neben den Aktivitäten unter dem Jahr den Höhepunkt einer jeden Jubla-Schar.</p>
             </div>
             <div>
                 <Counter targetNumber={150} />
                 <h1 className="text-blue-800 mb-5 text-5xl">Kurse</h1>
-                <p className='text-2xl'>In über 150 J+S- und Verbands­kursen bilden sich jährlich über 3'500 junge Leitende aus- und weiter. Die Kurse gestalten sich von Ehren­amtlichen für Ehren­amtliche.</p>
+                <p className='text-2xl'>In über 150 J+S- und Verbandskursen bilden sich jährlich über 3500 junge Leitende aus- und weiter. Die Kurse gestalten sich von Ehrenamtlichen für Ehrenamtliche.</p>
             </div>
             <div>
                 <Counter targetNumber={21} />
                 <h1 className="text-blue-800 mb-5 text-5xl">Kantone</h1>
-                <p className='text-2xl'>Die Jubla ist in 21 Kantonen der Deutsch­schweiz vertreten und in 15 Kantonal­verbände, die teilweise wiederum in Regionen unterteilt sind, aufgeteilt. Das Engagement in den Kantons- bzw. Regional­verbänden ist ehrenamtlich.</p>
+                <p className='text-2xl'>Die Jubla ist in 21 Kantonen der Deutschschweiz vertreten und in 15 Kantonalverbände, die teilweise wiederum in Regionen unterteilt sind, aufgeteilt. Das Engagement in den Kantons- bzw. Regionalverbänden ist ehrenamtlich.</p>
             </div>
         </div>
     </div>
