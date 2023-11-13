184bf1c6228cc3bd6b073da97fa2485be95df95d
Jungwacht <stantonjungwacht@gmail.com>
2023-11-13 08:44:35 +0100

    finalized Leiterliste

--- components/LeiterListe.jsx
+++ components/LeiterListe.jsx
@@ -38,7 +38,7 @@ const LeiterListe = () => {
                                     <div>
                                         <p className='text-2xl text-black px-3 mb-5'>{'Kurse: ' + leiter.node.kurse.join(', ')}</p>
                                         <p className='text-2xl text-black px-3 mb-2'>Email:</p>
-                                        <a href={`mailto:${leiter.node.email}`} className='text-2xl text-blue-800 hover:underline underline-offset-2 px-3 cursor-pointer mb-4'>{leiter.node.email}</a>
+                                        <a href={`mailto:${leiter.node.email}`} className='text-xl lg:text-2xl text-blue-800 hover:underline underline-offset-2 px-3 cursor-pointer mb-4'>{leiter.node.email}</a>
                                     </div>
                                 </div>
                             </div>   
@@ -46,30 +46,24 @@ const LeiterListe = () => {
                     </div>
                 </div>
             ))}
-            <div className='bg-white shadow-lg rounded-lg p-8 pb-8 mb-8'>
-                <div className='text-2xl text-blue px-3 mb-2'>
-                    <div className=''>
-                        <h3 className=' text-blue-800 transition duration-700 mb-1 text-4xl font-bold'>Unsere Aus- und Weiterbildung</h3>
-                        <p>Als Leitungspersonen besuchen wir regelmässig{' '}
-                            <span>  
-                                <a  href="https://www.jubla.ch/ueber-die-jubla/ausbildung-und-begleitung" className='text-2xl text-blue-800 hover:underline underline-offset-2 cursor-pointer mb-4'>Aus- und Weiterbildungskurse</a>
-                            </span>{' '}
-                            , die von Jungwacht Blauring in Zusammenarbeit mit{' '}
-                            <span>
-                                <a href="http://www.jugendundsport.ch/ rel=noopener target=_blank" className='text-2xl text-blue-800 hover:underline underline-offset-2 cursor-pointer mb-4'>Jugend+Sport (J+S)</a>
-                            </span>{' '}
-                            
-                            und weiteren Partnerorganisationen organisiert werden. Dabei lernen wir, kind- und jugendgerechte Freizeitgestaltung zu organisieren.
-                            </p>
-                        <br />
-                        <h3 className='text-blue-800 transition duration-700 mb-1 text-4xl font-bold'>Unterstützung durch Begleitpersonen</h3>
-                        <p>
-                            Wir erhalten von verschiedenen Begleit­personen Unterstützung. Dazu gehört ein*e <strong>Präses</strong>
-                            , eine <strong>Schar</strong>
-                            ­<strong>begleitung</strong>
-                            und für die Aktivität «Lager» zusätzlich ein*e <strong>J+S-Lagercoach</strong>.Dank unserem <strong>breiten Netzwerk</strong> dürfen wir auf die Unterstützung von vielen weiteren Personen zählen. Neben ehe­maligen Mitgliedern der Schar (frühere Leitungs­personen) stehen auch Personen aus dem näheren Umfeld und der Gemeinde (Pfarrei, Behörde, Schule, andere Vereine usw.) bei Bedarf mit Rat und Tat zur Seite.
+            <div className='text-2xl text-blue px-3 mb-2'>
+                <div className=''>
+                    <h3 className=' text-blue-800 transition duration-700 mb-1 text-4xl font-bold'>Unsere Aus- und Weiterbildung</h3>
+                    <p>Als Leitungspersonen besuchen wir regelmässig{' '}
+                        <span>  
+                            <a  href="https://www.jubla.ch/ueber-die-jubla/ausbildung-und-begleitung" className='text-2xl text-blue-800 hover:underline underline-offset-2 cursor-pointer mb-4'>Aus- und Weiterbildungskurse</a>
+                        </span>
+                        , die von Jungwacht Blauring in Zusammenarbeit mit{' '}
+                        <span>
+                            <a href="http://www.jugendundsport.ch/" className='text-2xl text-blue-800 hover:underline underline-offset-2 cursor-pointer mb-4'>Jugend+Sport (J+S)</a>
+                        </span>{' '}    
+                        und weiteren Partnerorganisationen organisiert werden. Dabei lernen wir, kind- und jugendgerechte Freizeitgestaltung zu organisieren.
                         </p>
-                    </div>
+                    <br />
+                    <h3 className='text-blue-800 transition duration-700 mb-1 text-4xl font-bold'>Unterstützung durch Begleitpersonen</h3>
+                    <p>
+                        Wir erhalten von verschiedenen Begleit­personen Unterstützung. Dazu gehört ein*e <strong>Präses</strong>, eine <strong>Scharbegleitung</strong> und für die Aktivität «Lager» zusätzlich ein*e <strong>J+S-Lagercoach</strong>. Dank unserem <strong>breiten Netzwerk</strong> dürfen wir auf die Unterstützung von vielen weiteren Personen zählen. Neben ehe­maligen Mitgliedern der Schar (frühere Leitungs­personen) stehen auch Personen aus dem näheren Umfeld und der Gemeinde (Pfarrei, Behörde, Schule, andere Vereine usw.) bei Bedarf mit Rat und Tat zur Seite.
+                    </p>
                 </div>
             </div>
         </div>
