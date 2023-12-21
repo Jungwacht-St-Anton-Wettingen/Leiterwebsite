0613fd40c244b90281d7690ee6757abc9a50caed
SirCraft007 <seramol@gmx.ch>
2023-12-21 13:55:30 +0100

    Bione

--- components/AccordionSection.jsx
+++ components/AccordionSection.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const AccordionSection = ({ title, content }) => {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  const toggleAccordion = () => {
-    setIsOpen(!isOpen);
-  };
+	const toggleAccordion = () => {
+		setIsOpen(!isOpen);
+	};
 
-  return (
-    <div className={`border-t-4 border-black p-2 transition-all duration-700 ease-in-out ${isOpen ? 'open' : ''}`}>
-      <style>
-        {`
+	return (
+		<div
+			className={`border-t-4 border-black p-2 transition-all duration-700 ease-in-out ${
+				isOpen ? "open" : ""
+			}`}
+		>
+			<style>
+				{`
           .accordion-content {
             max-height: 0;
             overflow: hidden;
@@ -21,16 +25,17 @@ const AccordionSection = ({ title, content }) => {
             max-height: 500px; /* Set a value that is greater than the expected height of your content */
           }
         `}
-      </style>
-      <div className="flex justify-between items-center cursor-pointer mt-3 mb-3" onClick={toggleAccordion}>
-        <h2 className={`text-2xl`}>{title}</h2>
-        <span className={`text-xl font-bold`}>{isOpen ? '-' : '+'}</span>
-      </div>
-      <div className={`mt-2 accordion-content`}>
-        {content}
-      </div>
-    </div>
-  );
+			</style>
+			<div
+				className="flex justify-between items-center cursor-pointer mt-3 mb-3"
+				onClick={toggleAccordion}
+			>
+				<h2 className={`text-2xl`}>{title}</h2>
+				<span className={`text-xl font-bold`}>{isOpen ? "-" : "+"}</span>
+			</div>
+			<div className={`mt-2 accordion-content`}>{content}</div>
+		</div>
+	);
 };
 
 export default AccordionSection;
--- components/LeiterListe.jsx
+++ components/LeiterListe.jsx
@@ -1,53 +1,67 @@
-import { getLeiter } from '@/services';
-import React, { useState, useEffect } from 'react';
+import { getLeiter } from "@/services";
+import React, { useEffect, useState } from "react";
 
 const LeiterListe = () => {
+	const [leiterListe, setLeiterListe] = useState([]);
 
-  const [leiterListe, setLeiterListe] = useState([])
+	useEffect(() => {
+		getLeiter().then((result) => setLeiterListe(result));
+	}, []); // leere Abhängigkeitsliste bedeutet, dass der Effekt nur beim Mounten der Komponente aufgerufen wird
 
-  useEffect(() => {
-    getLeiter()
-      .then((result) => setLeiterListe(result))
-  }, []); // leere Abhängigkeitsliste bedeutet, dass der Effekt nur beim Mounten der Komponente aufgerufen wird
-  
+	return (
+		<div className="container mx-auto px-0 lg:px-10 mb-8">
+			<div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+				<h1 className="text-blue-800 mb-5 text-4xl font-bold">
+					Leiter der Jungwacht St.Anton
+				</h1>
+				{leiterListe.map((leiter) => (
+					<div key={leiter.node.name}>
+						<div className="bg-white shadow-xl rounded-xl mb-8 p-0">
+							<div className="grid grid-cols-1 lg:grid-cols-10">
+								<div className="relative overflow-hidden shadow-md col-span-2">
+									<img
+										src={leiter.node.image.url}
+										alt={leiter.node.name}
+										className="object-top  h-full w-full object-cower shadow-lg rounded-t-xl lg:rounded-tr-none lg:rounded-l-xl"
+									/>
+								</div>
+								<div className="m-5 col-span-8">
+									<p className="text-4xl font-bold text-blue-800 mb-2 px-3">
+										{leiter.node.name}
+									</p>
+									<div className="grid grid-cols-1 lg:grid-cols-2">
+										<div>
+											<p className="text-2xl text-black px-3 mb-2">
+												{"Taufname: " + leiter.node.taufname}
+											</p>
+											<p className="text-2xl text-black px-3 mb-2">
+												{"Gruppe: " + leiter.node.gruppe.join(", ")}
+											</p>
+											<p className="text-2xl text-black px-3 mb-2">
+												{"Ämtli: " + leiter.node.aemtli.join(", ")}
+											</p>
+										</div>
+										<div>
+											<p className="text-2xl text-black px-3 mb-5">
+												{"Kurse: " + leiter.node.kurse.join(", ")}
+											</p>
+											<p className="text-2xl text-black px-3 mb-2">Email:</p>
+											<a
+												href={`mailto:${leiter.node.email}`}
+												className="text-xl lg:text-2xl text-blue-800 hover:underline underline-offset-2 px-3 cursor-pointer mb-4"
+											>
+												{leiter.node.email}
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
 
-  return (
-    <div className="container mx-auto px-0 lg:px-10 mb-8">
-        <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
-            <h1 className='text-blue-800 mb-5 text-4xl font-bold'>Leiter der Jungwacht St.Anton</h1>
-            {leiterListe.map((leiter) => (
-                <div key={leiter.node.name}>
-                    <div className='bg-white shadow-xl rounded-xl mb-8 p-0'>
-                        <div className='grid grid-cols-1 lg:grid-cols-10'>
-                            <div className='relative overflow-hidden shadow-md col-span-2'>
-                                <img 
-                                    src={leiter.node.image.url}
-                                    alt={leiter.node.name}
-                                    className='object-top  h-full w-full object-cower shadow-lg rounded-t-xl lg:rounded-tr-none lg:rounded-l-xl'
-                                  />
-                            </div>   
-                            <div className='m-5 col-span-8'>
-                                <p className='text-4xl font-bold text-blue-800 mb-2 px-3'>{leiter.node.name}</p>
-                                <div className='grid grid-cols-1 lg:grid-cols-2'>
-                                    <div>
-                                        <p className='text-2xl text-black px-3 mb-2'>{'Taufname: '+leiter.node.taufname}</p>
-                                        <p className='text-2xl text-black px-3 mb-2'>{'Gruppe: '+leiter.node.gruppe.join(', ')}</p>
-                                        <p className='text-2xl text-black px-3 mb-2'>{'Ämtli: ' + leiter.node.aemtli.join(', ')}</p>
-                                    </div>
-                                    <div>
-                                        <p className='text-2xl text-black px-3 mb-5'>{'Kurse: ' + leiter.node.kurse.join(', ')}</p>
-                                        <p className='text-2xl text-black px-3 mb-2'>Email:</p>
-                                        <a href={`mailto:${leiter.node.email}`} className='text-xl lg:text-2xl text-blue-800 hover:underline underline-offset-2 px-3 cursor-pointer mb-4'>{leiter.node.email}</a>
-                                    </div>
-                                </div>
-                            </div>   
-                        </div>            
-                    </div>
-                </div>
-            ))}
-        </div>
-    </div>
-  )
-}
-
-export default LeiterListe
+export default LeiterListe;
